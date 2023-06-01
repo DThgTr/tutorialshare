69de5f7b0cc3cc474652d9334e538516692db6f1
DThgTr <121519286+DThgTr@users.noreply.github.com>
2023-06-01 15:30:34 -0500

    setup project

---
+++ src/main/java/com/perpetualtutorial/tutorialshare/TutorialShareApplication.java
@@ -0,0 +1,13 @@
+package com.perpetualtutorial.tutorialshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perpetualtutorial/tutorialshare/TutorialshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perpetualtutorial.tutorialshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
